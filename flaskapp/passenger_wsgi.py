368047cf0c0d21a694d1cd01e20af93024076e7b
nelson.spencerg@gmail.com <138039144+SpencerGNelson@users.noreply.github.com>
2025-12-05 14:03:39 -0700

    adding wsgi

--- flaskapp/passenger_wsgi.py
+++ flaskapp/passenger_wsgi.py
@@ -1,8 +1,17 @@
-import sys
+import importlib.machinery
+import importlib.util
 import os
+import sys
 
-# Add your application directory to the Python path
+
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Import your Flask application
-from app import app as application
+def load_source(modname, filename):
+    loader = importlib.machinery.SourceFileLoader(modname, filename)
+    spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
+    module = importlib.util.module_from_spec(spec)
+    loader.exec_module(module)
+    return module
+
+wsgi = load_source('wsgi', 'app.py')
+application = wsgi.app
